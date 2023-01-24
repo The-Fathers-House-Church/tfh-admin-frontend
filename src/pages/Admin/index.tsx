e615f0b2888cc8652fc30835436ad8af93ddccba
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-01-24 12:22:19 +0100

    add admin

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -7,21 +7,21 @@ import AllAdmins from '../../components/Admin/AllAdmins';
 import AppLayout from '../../layout/AppLayout';
 
 function Admins() {
-	return (
-		<AppLayout
-		// pageAction={
-		// 	<Link to='/admin/new'>
-		// 		<Button className='max-w-[156px] !h-[40px] !p-4'>
-		// 			<FiPlus className='mr-3' />
-		// 			Add New
-		// 		</Button>
-		// 	</Link>
-		// }
-		>
-			<SectionHeader title='ALL ADMINS' />
-			<AllAdmins />
-		</AppLayout>
-	);
+  return (
+    <AppLayout
+      pageAction={
+        <Link to='/admin/new'>
+          <Button className='max-w-[156px] !h-[40px] !p-4'>
+            <FiPlus className='mr-3' />
+            Add New
+          </Button>
+        </Link>
+      }
+    >
+      <SectionHeader title='ALL ADMINS' />
+      <AllAdmins />
+    </AppLayout>
+  );
 }
 
 export default Admins;
